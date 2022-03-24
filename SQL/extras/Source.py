1ae0cd6b3aef7a60d9c2f8e8356b870ee82b3d70
UDFAST <102271062+UDFAST@users.noreply.github.com>
2022-03-24 12:24:06 +0200

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**🦅كيفيه التنصيب :**")
+help2 = ("**🦅قـائمـه الاوامـر :**\n**🦅قنـاه السـورس :** @FA9SH\n**🦅شـرح اوامـر السـورس : @l3ll3**\n**🦅شـرح فـارات السـورس : @teamtelethon** ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source safeina : @FA9SH  𓇡.**"
